e7e670f7056f63c1e08be128efd5a4937ec70941
Naveen <naveenkumar923@gmail.com>
2025-09-08 13:12:19 +0530

    changed tags to run

--- src/test/java/com/myProject/runners/CukesRunner.java
+++ src/test/java/com/myProject/runners/CukesRunner.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "com/myProject/stepDefinitions",
-        dryRun = false,
+//        dryRun = false,
         tags = "@mnk2"
 
 )
